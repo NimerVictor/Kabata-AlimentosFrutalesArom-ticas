1150f0ee6b783746d41da5e82142377188abe94d
Victor <utnprogramacionweb@gmail.com>
2024-08-01 12:58:15 -0600

    Se implementan las páginas de cada tipo de planta, se agrega el json de las plantas, el codigo js para cahar

---
+++ js/plantas.js
@@ -0,0 +1,43 @@
+window.onload = registrarPlantas;
+
+function registrarPlantas(){
+    fetch('../Json/plantas.json')
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+      let html = '';
+      let container_plantas = document.getElementById("container-plantas");
+      let tipo = container_plantas.accessKey;
+      container_plantas.innerHTML = "";
+      data.forEach(function(planta){
+        if (planta.Tipo == tipo) {
+          let propiedades = "";
+        planta.Propiedades.forEach(function(element, index){
+          if (index != (planta.Propiedades.length-1)) {
+            propiedades+= element + ", ";
+          } else {
+            propiedades+= element + ".";
+          }
+        });
+
+        html = `
+        <div class=" planta">
+          <div class="card">
+            <div class="image-wrapped">
+                      <img src="${planta.Imagen}" alt="...">
+            </div>
+            <div class="card-body ali plant-title">
+                      <h5 class="card-title">${planta.Nombre}</h5>
+                      <p class="card-text">Propiedades: ${propiedades}</p>
+            </div>
+            
+          </div>
+        </div>
+        `;
+        container_plantas.innerHTML += html;
+        }
+      });
+      
+    })
+}
