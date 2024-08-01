b0dbe6f5e1a5d7e350666ff23df8fb27e6c8bc13
Victor <utnprogramacionweb@gmail.com>
2024-08-01 17:35:55 -0600

    Se crean los filtros

--- js/plantas.js
+++ js/plantas.js
@@ -41,3 +41,53 @@ function registrarPlantas(){
       
     })
 }
+
+function FiltroPlantas(filtro){
+  fetch('../Json/plantas.json')
+  .then(function(res){
+      return res.json();
+  })
+  .then(function(data){
+    let acesso = false;
+    let html = '';
+    let container_plantas = document.getElementById("container-plantas");
+    let tipo = container_plantas.accessKey;
+    container_plantas.innerHTML = "";
+    data.forEach(function(planta){
+      if (planta.Tipo == tipo) {
+        let propiedades = "";
+      planta.Propiedades.forEach(function(element, index){
+        if(element == filtro){
+          acesso = true;
+        }
+        if (index != (planta.Propiedades.length-1)) {
+          propiedades+= element + ", ";
+        } else {
+          propiedades+= element + ".";
+        }
+      });
+
+      html = `
+      <div class=" planta">
+        <div class="card">
+          <div class="image-wrapped">
+                    <img src="${planta.Imagen}" alt="...">
+          </div>
+          <div class="card-body ali plant-title">
+                    <h5 class="card-title">${planta.Nombre}</h5>
+                    <p class="card-text">Propiedades: ${propiedades}</p>
+          </div>
+          
+        </div>
+      </div>
+      `;
+      if (acesso == true) {
+        container_plantas.innerHTML += html;
+        acesso = false
+      }
+      
+      }
+    });
+    
+  })
+}
